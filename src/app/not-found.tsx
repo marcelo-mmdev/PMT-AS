fd69660fec598a03da54b059dab7220c89b50c03
marcelo-mmdev <manoelmarcelo.r@gmail.com>
2025-08-21 01:52:52 -0300

    Feat: Ajuste 6

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,29 +1,16 @@
 import Link from "next/link"
-//import { auth } from "@/auth" // usamos a sessão
-//import { Button } from "@/components/ui/button" // caso tenha shadcn/ui, se não pode usar <a>
-import { auth } from "@/types/auth"
 
-export default async function NotFoundPage() {
-  const session = await auth()
-
+export default function NotFoundPage() {
   return (
     <main className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
       <h1 className="text-6xl font-bold mb-4 text-red-600">404</h1>
       <p className="text-lg text-gray-700 mb-6">Página não encontrada</p>
 
-      {!session ? (
-        <Link href="/login">
-          <button className="px-6 py-2">
-            Ir para Login
-          </button>
-        </Link>
-      ) : (
-        <Link href="/dashboard">
-          <button className="px-6 py-2">
-            Ir para Dashboard
-          </button>
-        </Link>
-      )}
+      <Link href="/login">
+        <button className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition">
+          Ir para Login
+        </button>
+      </Link>
     </main>
   )
 }
